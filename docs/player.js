a42323f5cbeec629122c3bf7efc5a8afce8af226
SunochinKO <keiouniv@net90-dhcp-149.sfc.keio.ac.jp>
2017-05-18 12:23:37 +0900

    順番に出していくマンに勝つ
    順番に出していくマンに勝つのを追加

--- docs/player.js
+++ docs/player.js
@@ -4,8 +4,6 @@ const PA = 2;
 
 let matches = 0;
 
-
-
 //試合数を数える関数
 function countup(){
   matches = matches + 1
@@ -15,7 +13,8 @@ function countup(){
 /* 対戦相手名前まとめ
 チョキ大好きマン：　fighter::choki-lover
 表か裏かマン： fighter::odd-even
-帰ってきた表か裏かマン: fighter::onthird
+帰ってきた表か裏かマン: fighter::on-third
+順番に出していくマン：　fighter::rotation
  */
 
 
@@ -25,14 +24,15 @@ function actionAgainstChokiDaisukiman(){
 }
 
 //表か裏かマンに勝つ関数
-
 function actionAgainstOmoteUra(){
   const remainder = matches % 2;
   const isEven = remainder == 0;
   if(isEven){
-  return GU;
-}
-return PA;
+    return GU;
+  }
+  else{
+    return PA;
+  }
 }
 
 
@@ -41,10 +41,28 @@ function actionAgainstOmoteUra2(){
   const remainder = matches % 3;
   const isEven = remainder == 0;
   if(isEven){
+    return CHOKI;
+  }
+  else{
+    return GU;
+  }
+}
+
+
+//順番に出していくマンに勝つ関数
+ function actionAgainstRotation(){
+   const remainder = matches % 3;
+   if (remainder == 1){
+     return PA;
+    }
+  else if (remainder== 2){
+    return GU;
+  }
+else {
   return CHOKI;
 }
-return GU;
 }
+ 
 
 //対戦に関する記述
 function action(oppornent){
@@ -57,10 +75,14 @@ if (oppornent =="fighter::choki-lover"){
 else if (oppornent == "fighter::odd-even"){
   result = actionAgainstOmoteUra();
 }
-else {
+else if (oppornent == "fighter::on-third"){
   result = actionAgainstOmoteUra2();
+}
+else if (oppornent == "fighter::rotation"){
+  result = actionAgainstRotation();
 }
 
 return result;
 }
 
+console.log(oppornent);
